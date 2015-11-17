14a872b1a30d03115996cbcc48fd84b934793ea8
xinrf1 <xinrf1@lenovo.com>
2015-11-17 09:34:50 +0800

    menu update

--- ngDirectives/menu-list.directive.js
+++ ngDirectives/menu-list.directive.js
@@ -3,55 +3,49 @@ define(["require","angular"], function(require,ng) {
 	module.directive('menuList',['$location','$q','$http','funcUtils','httpUtils','$rootScope','$state',function($location,$q,$http,funcUtils,httpUtils,$rootScope,$state){
 		return {
 			trstrice:"EA",
-			scope:{
-				url:"@"
-			},
+			/*scope:{
+				url:"@",
+                menudata:"@"
+			},*/
+			controller:"layoutCtrl",
 			templateUrl:cmpConfig.directivesPath+"menu-list.html",
 			link:function(scope,ele,attrs){
 				if($location.search()["debug"]){
 					scope.url = "../testdata/menu.json";
 				}
-				
-				//如果测试通过请求静态资源文件，否则通过后台接口url形式请求menu 菜单
-				if(scope["url"].indexOf(cmpConfig.apiTestUrl)==-1){
-					scope.url = cmpConfig.apiUrl+scope.url;
-				}
-
-				httpUtils.httpGet(scope.url).then(function(data) {
-		            scope.menus = data.resultObject || data;
-		            if("app"!=$state.current.name){
-		            	_setActive(scope.menus,$state.current.name + "(" + JSON.stringify($state.params).replace(/"/g,'\'') + ")");  
-		            }
-		        }, function() {
-		             scope.menus = [];
-		        });   
-		    	
 		        $rootScope.$on('$stateChangeStart', 
 				function(event, toState, toParams, fromState, fromParams){ 
-					var url = toState.name + "(" + JSON.stringify(toParams).replace(/"/g,'\'') + ")";
-				    _setActive(scope.menus,url);
+				/*	httpUtils.httpGet(tempUrl).then(function(data) {
+		            scope.menus = data.resultObject || data;
+		            if("app"!=$state.current.name)
+		            	_setActive(scope.menus,$state.current.name);  
+		        }, function() {
+		             scope.menus = [];
+		        }); */
+		        console.log(scope.menus)
+				    _setActive(scope.menus,toState.name);
 				}); 
+
 				scope.expand=function(menu){
-					menu.expand=!menu.expand;
-					if(!menu.params) return;
-					var params = menu.params.replace(/'/g,'"');
-					if(menu.goto){
-						$state.go(menu.urlState,angular.fromJson(params));
-					}
+					menu.expand=!menu.expand; 
 				}
 				_checkUrl=function(state,url){
 					return url.indexOf(state)!=-1 
 				};
 				_setActive=function(children,url){
-					$.each(children,function(i,v){
-						v.children = v.children || v.childen;
-		        		v.activate=false; 
-		        		if(_checkUrl(v.href,url)&&(!v.children || v.children.length == 0))
-							v.activate=true;
-		        		if(v.children&&v.children.length>0){
-		        			_setActive(v.children,url);
-		        		}
-		        	});
+					console.log(children);
+					if(children&&children!=''&&typeof (children)!='undefined'){
+						$.each(children,function(i,v){
+							v.children = v.children || v.childen;
+							v.activate=false;
+							if(_checkUrl(v.href,url)&&(!v.children || v.children.length == 0))
+								v.activate=true;
+							if(v.children&&v.children.length>0){
+								_setActive(v.children,url);
+							}
+						});
+					}
+
 				};   
 			}
 		}
