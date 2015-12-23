754e00c708d063c2a48c3c81e18bee5fb3579eb9
xulu3 <xulu3@lenovo.com>
2015-12-23 10:58:22 +0800

    add MAX_NUMBER:Math.pow(2,31)-1

--- ngServices/app-utils.module.js
+++ ngServices/app-utils.module.js
@@ -7,7 +7,8 @@ define(["require","angular","angular-grid"], function(require,ng) {
         "E_MAIL":/^([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$/i,
         "DISPLAY_NAME":/^[0-9a-zA-Z_\-]+$/,
         "REGEXP_DESC": /^[^/\\\\…….]+$/,
-        "POSITIVE_INTEGER":/^[0-9]*[1-9][0-9]*$/
+        "POSITIVE_INTEGER":/^[0-9]*[1-9][0-9]*$/,
+        "MAX_NUMBER":Math.pow(2,31)-1,
     })
 	
 	return module;
